7d1eafb85c746f7f01ab8c6c8d345dd3f0f2c369
xelAraY <alexey.yarohovich18@gmail.com>
2022-03-13 01:34:52 +0300

    Добавьте файлы проекта.

---
+++ TI_Lab2/LFSR method.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI_Lab2
+{
+    static class LFSR_method
+    {
+        public const int REGISTER_SIZE = 25;
+        static byte[] register = new byte[REGISTER_SIZE];      
+        
+        private static void FillRegister(string initStr)
+        {
+            for(int i = 0; i < initStr.Length; i++)
+            {
+                register[i] = byte.Parse(initStr[i].ToString());
+            }   
+        }
+
+        public static string TextFromBin(List<byte> bytes)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                string tmp = Convert.ToString(b, 2);
+                sb.Append("00000000"[..(8 - tmp.Length)] + tmp);
+            }
+            return sb.ToString();
+        }
+
+        private static byte ShiftReg(byte[] reg)
+        {
+            byte olderBit = reg[0];
+            byte oldThirdBit = reg[22];
+
+            for (int i = 0; i < REGISTER_SIZE - 1; i++)
+                reg[i] = reg[i + 1];
+            
+            reg[REGISTER_SIZE - 1] = (byte)(olderBit^oldThirdBit);
+
+            return olderBit;
+        }
+
+        public static List<byte> FormKey(string initStateStr, int length)
+        {
+            List<byte> key = new List<byte>();
+            FillRegister(initStateStr);
+            byte[] shiftRegister = register;
+            int i = 0;
+
+            while (length > 0)
+            {
+                key.Add(ShiftReg(shiftRegister));
+                byte b = 0;
+                int count = 7;
+                
+                while (length > 0 && count > 0)
+                {
+                    key[i] = (byte)(key[i] << 1);
+                    b = ShiftReg(shiftRegister);
+                    key[i] |= b;
+                    count--;
+                    length--;                   
+                }
+                i++;
+                length--;
+            }
+            return key;
+        }
+
+        public static byte[] BinTextForm(string plaintext)
+        {
+            byte b = 0;
+            int j = 0;
+            int binTextSize = plaintext.Length / 8; 
+            byte[] binText = new byte[binTextSize];
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                binText[j] = byte.Parse(plaintext[i].ToString());
+
+                int count = 7;
+                while (i < plaintext.Length - 1 && count > 0)
+                {
+                    binText[j] = (byte)(binText[j] << 1);
+                    i++;
+                    b = byte.Parse(plaintext[i].ToString());
+                    binText[j] |= b;
+                    count--;
+                }
+                j++;
+            }
+            return binText;
+        }
+
+        public static List<byte> Encrypt(byte[] plaintextBin, byte[] keyBin)
+        {
+            List<byte> resultBin = new List<byte>();
+
+            for(int i = 0; i < plaintextBin.Length; i++)
+                resultBin.Add((byte)(plaintextBin[i]^keyBin[i]));
+            return resultBin;
+        }
+    }
+}
---
+++ TI_Lab2/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace TI_Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private static string _filePath = String.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();      
+
+            if (ofd.ShowDialog() == true)
+            {
+                _filePath = ofd.FileName;
+                byte[] bytes = File.ReadAllBytes(ofd.FileName);
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    string tmp = Convert.ToString(b, 2);
+                    sb.Append("00000000"[..(8 - tmp.Length)] + tmp);
+                }
+
+                Plaintext.Text = sb.ToString();
+            }
+        }
+
+        private void Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            if (Key.Text == String.Empty)
+            {
+                MessageBox.Show("Ошибка. Необходимо сгенерировать ключ шифрования!");
+                return;
+            }
+            ResultText.Text = LFSR_method.TextFromBin(LFSR_method.Encrypt(
+                LFSR_method.BinTextForm(Plaintext.Text), LFSR_method.BinTextForm(Key.Text)));
+
+            string fileName = System.IO.Path.GetDirectoryName(_filePath) + '\\' + "New_" +
+                System.IO.Path.GetFileName(_filePath);
+            File.WriteAllBytes(fileName, LFSR_method.BinTextForm(ResultText.Text));
+        }
+
+        private void GenerateKey_Click(object sender, RoutedEventArgs e)
+        {
+            if (InitialString.Text == String.Empty)
+            {
+                MessageBox.Show("Ошибка. Необходимо ввести начальное состояние для регситра!");
+                return;
+            }
+
+            if (Plaintext.Text == String.Empty)
+            {
+                MessageBox.Show("Ошибка. Необходимо выбрать файл для шифрования!");
+                return;
+            }
+
+            foreach(char c in InitialString.Text)
+            {
+                if (c != '0' && c != '1')
+                {
+                    MessageBox.Show("Ошибка. Необходимо вводить только 0 или 1");
+                    return;
+                }                 
+            }
+
+            if (InitialString.Text.Length != LFSR_method.REGISTER_SIZE)
+            {
+                MessageBox.Show("Ошибка. Число разрядов регистра - 25!");
+                return;
+            }
+
+            Key.Text = LFSR_method.TextFromBin(LFSR_method.FormKey(
+                InitialString.Text, Plaintext.Text.Length));          
+        }
+    }   
+}
